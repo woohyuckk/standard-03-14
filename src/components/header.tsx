1f1f5866f44ac0f91b3f73c10aeacf8c6ed7aed0
woohyuckk <woohyuckk@gmail.com>
2025-03-14 11:37:07 +0900

    1단계 완료

---
+++ src/components/header.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <>
+      <nav className="navbar">
+        <ul className="nav-list flex text-2xl ">
+          <li className="nav-item">
+            <Link href="/" className="nav-link">
+              Home(SSG)
+            </Link>
+          </li>
+
+          <li className="nav-item">
+            <Link href="/news" className="nav-link">
+              News(ISR)
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/blogs" className="nav-link">
+              Blogs(SSR)
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/todolist" className="nav-link">
+              TodoList(CSR)
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
