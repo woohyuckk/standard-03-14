2316480066293529128487b5d583caa6036f40a1
woohyuckk <woohyuckk@gmail.com>
2025-03-14 11:52:48 +0900

    2단계 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,31 @@
-export default function Home() {
-  return <div>Home</div>;
+interface IntroType {
+  description1: string;
+  description2: string;
+  description3: string;
+}
+
+export default async function Home() {
+  const data = await fetch("http://localhost:4000/intro", {
+    cache: "force-cache",
+  });
+  const introData: IntroType = await data.json();
+  return (
+    <div className="home-container">
+      <div className="intro-section">
+        <h1 className="intro-title">React to Next.js 마이그레이션 프로젝트</h1>
+        <div className="description-text">
+          <p>
+            SSG 페이지는 빌드 시점에 데이터를 미리 가져와서 캐싱 후, 사용자 요청
+            시 캐싱해둔 데이터로 페이지를 렌더링하는 방식입니다.
+          </p>
+          <p>변경되지 않는 정적인 페이지에 적절한 렌더링 방식입니다.</p>
+        </div>
+        <div className="intro-descriptions">
+          <p className="description">{introData.description1}</p>
+          <p className="description">{introData.description2}</p>
+          <p className="description">{introData.description3}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/todolist/page.tsx
+++ src/app/todolist/page.tsx
@@ -1,3 +1,12 @@
+interface Todos {
+  id: string;
+  title: string;
+  contents: string;
+  isCompleted: boolean;
+  imgPath: string;
+  createdAt: number;
+}
+
 const TodoListPage = () => {
   return <div>TodoListPage(CSR)</div>;
 };
